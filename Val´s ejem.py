750d4eb0e8528bacbab93a1a878516974ff2592e
valen042012 <valentina.santamaria0104@uhispano.ac.cr>
2025-01-20 19:18:50 -0600

    Primer commit

---
+++ Val´s ejem.py
@@ -0,0 +1,16 @@
+# Esto es un comentario 
+varComentario = """
+        Esto es un comentario 
+
+        
+"""
+
+varNum = 8522
+varPalabra = "Palabra"
+print(" hola mundo ")
+print(varNum)
+print(varPalabra)
+print(varComentario)
+
+
+
